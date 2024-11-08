c5d8397aa9079df82f9e2893f088efb9563ad37f
smlhng <sml.hng@gmail.com>
2024-11-07 20:44:44 -0800

    feat: do the tail ex

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -47,7 +47,11 @@ object List: // `List` companion object. Contains functions for creating and wor
   def productViaFoldRight(ns: List[Double]): Double =
     foldRight(ns, 1.0, _ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = {
+    l match
+      case Nil => Nil
+      case _ :: t => t
+  }
 
   def setHead[A](l: List[A], h: A): List[A] = ???
 
